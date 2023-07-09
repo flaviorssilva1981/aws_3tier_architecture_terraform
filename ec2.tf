031426a69ac0b6e89dbf5a4959afb63698c1cecb
flaviorssilva1981 <flavio.rssilva@gmail.com>
2023-07-09 08:48:05 -0300

    changed key name to lab01

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0507f77897697c4ba"
   instance_type = "t2.micro"
-  key_name = "pswain"
+  key_name = "lab01"
   subnet_id = aws_subnet.public[count.index].id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   associate_public_ip_address = true
@@ -18,14 +18,14 @@ resource "aws_instance" "web" {
   }
 
   provisioner "file" {
-    source = "./pswain.pem"
-    destination = "/home/ec2-user/pswain.pem"
+    source = "./lab01.pem"
+    destination = "/home/ec2-user/lab01.pem"
   
     connection {
       type = "ssh"
       host = self.public_ip
       user = "ec2-user"
-      private_key = "${file("./pswain.pem")}"
+      private_key = "${file("./lab01.pem")}"
     }  
   }
 }
@@ -33,7 +33,7 @@ resource "aws_instance" "web" {
 resource "aws_instance" "db" {
   ami           = "ami-0507f77897697c4ba"
   instance_type = "t2.micro"
-  key_name = "pswain"
+  key_name = "lab01"
   subnet_id = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.allow_tls_db.id]
 
